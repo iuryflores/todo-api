57b623216c349b9f12e761a916ca4e14e123e43c
iuryflores <iury.flores@gmail.com>
2022-10-29 15:18:14 -0300

    Iteration 2 done

--- models/Todo.js
+++ models/Todo.js
@@ -1,5 +1,10 @@
 import { Schema, model } from "mongoose";
 
 const todoSchema = new Schema({
-    
-})
+    title: {
+        type: String, required: [true, 'Title is required!'],
+    },
+    completed: {
+        type: Boolean, default: [false]
+    }
+}, { timestamps: true })
