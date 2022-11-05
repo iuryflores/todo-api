522270b7b986565925728f25e49687b77bfb7c8b
unknown <iury.flores@gmail.com>
2022-11-05 16:17:46 -0300

    Fix: Todo unique

--- app.js
+++ app.js
@@ -1,65 +1,71 @@
-import express from 'express'
-import './config/db.config.js'
-import Todo from './models/Todo.js'
-import cors from 'cors'
+import express from "express";
+import "./config/db.config.js";
+import Todo from "./models/Todo.js";
+import cors from "cors";
 
-const app = express()
+const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 //Homepage
-app.get('/', (req, res) => {
-    res.send("It's working!")
-})
+app.get("/", (req, res) => {
+  res.send("It's working!");
+});
 
 //Get all Todos
-app.get('/todos', async (req, res) => {
-    const allTodos = await Todo.find()
-    res.status(200).json(allTodos)
-    
-})
+app.get("/todos", async (req, res) => {
+  const allTodos = await Todo.find();
+  res.status(200).json(allTodos);
+});
 
 //Post Todo
-app.post('/todos', async (req, res) => {
-    const { body } = req
-    try {
-        const newTodo = await Todo.create(body)
-        res.status(201).json(newTodo)
-    } catch (error) {
-        res.status(400).json({ status: 400, msg: error.message })
+app.post("/todos", async (req, res) => {
+  const { title } = req.body;
+  const { body } = req;
+
+  try {
+    const todoFind = await Todo.findOne({ title: title });
+    if (todoFind) {
+      return res.status(404).json({ msg: "Encontrado" });
     }
-})
+
+    const newTodo = await Todo.create(body);
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(400).json({ status: 400, msg: error });
+  }
+});
 
 //Update Todo
-app.put('/todos/:id', async (req, res) => {
-    const { id } = req.params
-  
-    try {
-        const old = await Todo.findById(id)
-        const update = {completed: `${!old.completed}` }
-        const todo = await Todo.findByIdAndUpdate(id, update, { new: true })
-        res.status(201).json(update)
-    } catch (error) {
-        res.status(400), json({ status: 400, msg: error.message })
-    }
-})
+app.put("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const old = await Todo.findById(id);
+    const update = { completed: `${!old.completed}` };
+    const todo = await Todo.findByIdAndUpdate(id, update, { new: true });
+    res.status(201).json(update);
+  } catch (error) {
+    res.status(400), json({ status: 400, msg: error.message });
+  }
+});
 
 //Delete Todo
-app.delete('/todos/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        const todo = await Todo.findByIdAndRemove(id)
-        if (todo) {
-            res.status(201).json({ msg: `${todo.title} deletado com sucesso` })
-        } else {
-            res.status(404).json({ msg: 'Todo not found' })
-        }
-    } catch (error) {
-        res.status(400), json({ status: 400, msg: error.message })
+app.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const todo = await Todo.findByIdAndRemove(id);
+    if (todo) {
+      res.status(201).json({ msg: `${todo.title} deletado com sucesso` });
+    } else {
+      res.status(404).json({ msg: "Todo not found" });
     }
-})
+  } catch (error) {
+    res.status(400), json({ status: 400, msg: error.message });
+  }
+});
 
 //Listening
 app.listen(3000, () => {
-    console.log(`Server running on http://localhost:3000 port`)
-})
+  console.log(`Server running on http://localhost:3000 port`);
+});
--- models/Todo.js
+++ models/Todo.js
@@ -1,13 +1,19 @@
 import { Schema, model } from "mongoose";
 
-const todoSchema = new Schema({
+const todoSchema = new Schema(
+  {
     title: {
-        type: String, required: [true, 'Title is required!'],
+      type: String,
+      required: [true, "Title is required!"],
+      uniqued: true,
     },
     completed: {
-        type: Boolean, default: false
-    }
-}, { timestamps: true })
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const Todo = model('Todo', todoSchema)
-export default Todo
+const Todo = model("Todo", todoSchema);
+export default Todo;
